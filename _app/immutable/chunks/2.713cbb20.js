fa6f5f07563a24c3a4be3e56a7a55680cace28a5
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-04-09 10:31:25 -0400

    center align projects

---
+++ _app/immutable/chunks/2.713cbb20.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.25e6279b.js";export{t as component};
--- _app/immutable/chunks/2.951c233d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.a1dae742.js";export{t as component};
