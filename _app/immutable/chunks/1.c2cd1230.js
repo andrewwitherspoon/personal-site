5575c0b8a2ae89611fbf886e108f81e08ec3658b
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-04-09 22:36:42 -0400

    footer animation

---
+++ _app/immutable/chunks/1.c2cd1230.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.2de51270.js";export{t as component};
--- _app/immutable/chunks/2.713cbb20.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.25e6279b.js";export{t as component};
