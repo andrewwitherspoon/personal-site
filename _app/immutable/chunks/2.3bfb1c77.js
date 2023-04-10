3a90602c66375473fb584817200ba315ca99e6c6
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-04-10 08:36:43 -0400

    add ball spin

---
+++ _app/immutable/chunks/2.3bfb1c77.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.3abcb063.js";export{t as component};
--- _app/immutable/chunks/2.e5735b41.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.cf98b467.js";export{t as component};
