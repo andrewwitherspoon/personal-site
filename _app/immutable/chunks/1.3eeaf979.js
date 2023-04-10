d19806aa6842892c6b59cce3ae724e0fa704b376
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-04-10 00:14:00 -0400

    gonna have to learn how to squash without a force push

--- _app/immutable/chunks/1.3eeaf979.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.b42e7811.js";export{t as component};
---
+++ _app/immutable/chunks/2.e5735b41.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.cf98b467.js";export{t as component};
