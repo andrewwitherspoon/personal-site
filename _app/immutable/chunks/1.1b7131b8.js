ec126dcae7df9457509c34be555b5b1b03cf214c
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-04-09 23:33:28 -0400

    tweaking

--- _app/immutable/chunks/1.1b7131b8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.d79c7427.js";export{t as component};
--- _app/immutable/chunks/2.bbacd051.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.533e25fc.js";export{t as component};
