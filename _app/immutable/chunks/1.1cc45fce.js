46a7c5536d86261ead3853988a23898ed6bc1942
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-05-12 13:22:18 -0400

    fixed weird hover bug onpersonal site

--- _app/immutable/chunks/1.1cc45fce.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.fe9ded51.js";export{t as component};
--- _app/immutable/chunks/2.3f3d2bc2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.91b7c413.js";export{t as component};
