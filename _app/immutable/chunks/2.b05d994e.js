ec237f4fb2631d2f6e9517e7b925f56fed0bcc59
Andrew Witherspoon <andrew@andrewwitherspoon.com>
2023-05-12 14:59:56 -0400

    fixed animation on mobile

--- _app/immutable/chunks/2.b05d994e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.c43a9961.js";export{t as component};
---
+++ _app/immutable/chunks/2.f042098a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.6356068b.js";export{t as component};
